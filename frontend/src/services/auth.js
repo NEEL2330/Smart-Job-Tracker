7caa2898f450389ef8318d1fa2373195b1caa144
Neel <neelbera1107@gmail.com>
2026-01-07 17:34:13 +0530

    changes

--- frontend/src/services/auth.js
+++ frontend/src/services/auth.js
@@ -1,13 +1,13 @@
 import API from "./api";
 
 export const loginUser = async (email, password) => {
-  const res = await API.post("/api/auth/login", { email, password });
+  const res = await API.post("/auth/login", { email, password });
   localStorage.setItem("token", res.data.access_token);
   return res.data;
 };
 
 export const registerUser = async (name, email, password) => {
-  const res = await API.post("/api/auth/register", { name, email, password });
+  const res = await API.post("/auth/register", { name, email, password });
   localStorage.setItem("token", res.data.access_token);
   return res.data;
 };
